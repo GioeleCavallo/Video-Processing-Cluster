503e47b0791b06b96f8304f173b74e125f3a79f8
GioeleCavallo <gioele.cavallo@samtrevano.ch>
2022-03-24 13:19:45 +0000

    Gioele Cavallo 24.03.2022

---
+++ 5_Sito o applicativo/htdocs/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link href="css/css.css" rel="stylesheet" type="text/css">
+        <link href="dropzone.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+		<h1 class="title">UPLOAD</h1>
+		<hr class="title_divisor">
+
+		<div class="container" >
+		<form method="POST" action="upload.php" enctype="multipart/form-data" class="dropzone" id="dropzonewidget" >
+			<input max type="file" name="file" accept="video/mp4" >
+	               	<input type="submit" name="submit">
+                 </form>
+        	</div> 
+	</body>
+</html>
