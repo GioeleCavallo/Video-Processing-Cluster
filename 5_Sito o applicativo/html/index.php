3db03878661c4dd9083269c80079254d83138751
SamueleAbba <59683355+SamueleAbba@users.noreply.github.com>
2022-04-28 16:09:33 +0200

    add samuele abba
    ...

--- 5_Sito o applicativo/html/index.php
+++ 5_Sito o applicativo/html/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $servers = explode(",", ini_get("session.save_path"));
 $c = count($servers);
 for ($i = 0; $i < $c; ++$i) {
@@ -11,22 +11,24 @@ $mem->addServers($servers);
 <html>
 <head>
 	<link href="css/style.css" rel="stylesheet" type="text/css">
-        <link href="css/css.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	<h1>SERVER 1</h1>
-	<h1 class="title">UPLOAD</h1>
-	<hr class="title_divisor">
-	<form action="upload.php" method="POST">
-		<p>insert token:</p>
-		<input type="number" name="id">
-		<input type="submit" name="submitToken">
-	</form>
-	<div class="container" >
-		<form method="POST" action="upload.php" enctype="multipart/form-data" class="dropzone" id="dropzonewidget" >
-			<input max type="file" name="file" accept="video/mp4" >
-	      		<input type="submit" name="submit">
+	<h1 style="margin: 0px;">SERVER 1</h1>
+	<div class="main">
+		<div>
+			<h1 class="title">UPLOAD</h1><hr class="title_divisor"><br>
+			<form action="upload.php" method="POST" class="subContainer">
+				<input type="number" name="id" placeholder="Inserire un token:" min="0" max="9999999999" class="other">
+				<input type="submit" name="submitToken" value="Mostra" id="show">
+			</form>
+		</div>
+		<br>
+		<div>
+			<form method="POST" action="upload.php" class="subContainer">
+				<input type="file" name="file" accept="video/mp4" class="other">
+	     		<input type="submit" name="submit" value="Invia" id="send">
         	</form>
         </div>
+	</div>
 </body>
 </html>
