f961a5d16a492e06d3cfbb93a594b0404c4f3dec
GioeleCavallo <59683202+GioeleCavallo@users.noreply.github.com>
2022-04-28 16:20:11 +0200

    Aggiunto le pagine di errore
    GASEM

--- 5_Sito o applicativo/html/upload.php
+++ 5_Sito o applicativo/html/upload.php
@@ -7,6 +7,7 @@ for ($i = 0; $i < $c; ++$i) {
 }
 $mem = new Memcached();
 $mem->addServers($servers);
+$error_dir = "error/";
 
 $handlerInfo = fopen("/var/www/log/info.log","a");
 $handlerError = fopen("/var/www/log/error.log","a");
@@ -20,16 +21,15 @@ if (isset($_POST["submit"])) {
     $uploadOk = 1;
     $maxSize = 50000000;
     if ((strtolower(pathinfo($target_file, PATHINFO_EXTENSION))) != "mp4") {
-        echo "File is not a video!<br>";
+        require $error_dir."file_not_video.html";
         $uploadOk = 0;
     }
     if ($_FILES['file']['size'] > $maxSize) {
-        echo "File is too large!  <br>";
+        require $error_dir."file_too_large.html";
         $uploadOk = 0;
     }
     if (!file_exists($target_dir)) {
-        echo "Folder: $target_dir created successfully!<br>";
-        mkdir($target_dir, 0777, true);
+       	mkdir($target_dir, 0777, true);
     }
     if ($uploadOk == 1) {
         if (!file_exists("$target_dir$time/")) {
@@ -37,7 +37,6 @@ if (isset($_POST["submit"])) {
             chmod("$target_dir$time/", 0777);
         }
         if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-            echo "The file " . htmlspecialchars(basename($_FILES["file"]["tmp_name"])) . " has been uploaded.<br>";
             $target_txt = "$target_dir$time/testo.txt";
             $shell_exec_video = "sh cmd.sh '$target_file' '$target_txt' '$target_dir$time/'";
             echo shell_exec($shell_exec_video);
@@ -51,20 +50,17 @@ if (isset($_POST["submit"])) {
         } else {
 			$txt = date("d.m.y H:i:s")." : ERROR : failed upload of ".$time." from ".$_SERVER['REMOTE_ADDR'] . PHP_EOL;
 			fwrite($handlerError,$txt);
-            echo "Sorry, there was an error uploading your file.<br>";
+            require $error_dir."upload_failed.html";
         }
     } else {
-        echo "Please try again!<br>";
+        require $error_dir."try_again.html";
     }
 } elseif (isset($_POST["submitToken"])) {
     $GLOBALS["token"] = $mem->get($_POST["id"]);
-    echo "$value<br>";
     if ($GLOBALS["token"] != $null) {
-        echo "it works";
         require "stats/stats.php";
-        echo "ok";
     } else {
-        echo "non ok";
+        require $error_dir."invalid_token.html";
     }
 }
 fclose($handler);
